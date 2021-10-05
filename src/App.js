97d8a97535bb2509fa20d2efb856d1d1b71c6aff
jsr25 <ramosjuans@live.com.ar>
2021-10-04 19:49:44 -0500

    Complete first part

--- src/App.js
+++ src/App.js
@@ -8,13 +8,16 @@ function App() {
 
         <ul>
           <li>
-
+          <input checked="true" type="checkbox"/>
+          <span>Learn React</span>
           </li>
           <li>
-
+          <input type="checkbox"/>
+          <span>Lear Hooks</span>
           </li>
           <li>
-            
+          <input type="checkbox"/>
+          <span>Keep on Keeping on</span>  
           </li>
         </ul>
     </main>
