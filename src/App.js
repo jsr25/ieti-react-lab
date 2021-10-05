12f22122a44ec6a4b3eb292dc2bc6eddad47bbd7
jsr25 <ramosjuans@live.com.ar>
2021-10-04 20:38:34 -0500

    Add hook on app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import {TaskItem} from "./Taskitems"
+import {useState} from "react";
+import {TaskItem} from "./Taskitems";
 
 function App() {
+  const [task, setTasks] = useState([
+    {
+    isCompleted:true,
+    name:"Learn React",
+  },
+  {
+    isCompleted:true,
+    name:"Learn Hooks",
+  },
+  {
+    isCompleted:true,
+    name:"Keep on Keeping on",
+  },
+  ]);
+
+
+  const handleTaskChange =()=>{
+    console.log("changed!")
+  }
+
   return(
     <main>
         <form>
@@ -8,9 +29,9 @@ function App() {
           <button>Create Task</button>
         </form>
         <ul>
-          <TaskItem isChecked={true} taskName="Learn React"/>
-          <TaskItem isChecked={true} taskName="Lear Hooks"/>
-          <TaskItem isChecked={true} taskName="Keep on Keeping on"/>
+          <TaskItem isChecked={true} taskName="Learn React" onTaskChange={handleTaskChange}/>
+          <TaskItem isChecked={false} taskName="Lear Hooks" onTaskChange={handleTaskChange}/>
+          <TaskItem isChecked={true} taskName="Keep on Keeping on" onTaskChange={handleTaskChange}/>
         </ul>
     </main>
   );
